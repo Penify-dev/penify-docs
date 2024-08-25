89adf2952525326906aea635a72463b38a8a9ac9
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-08-25 20:14:18 +0530

    feat: Add ShareButtons component for social media sharing

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <template #doc-footer-before>
+      <ShareButtons />
+    </template>
+  </Layout>
+</template>
+
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import ShareButtons from './ShareButtons.vue'
+
+const { Layout } = DefaultTheme
+</script>
